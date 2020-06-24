0ec61ac16eaec5041fc6fd96af6c87dc88658a38
KConlon13 <KyleConlon13@gmail.com>
2020-06-24 12:09:57 -0400

    Netlify test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather Hack App deployed on Heroku Yoooo</h1>
+      <h1>Weather Hack App deployed on Heroku Yoooo Son</h1>
     </div>
   );
 }
